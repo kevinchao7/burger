c432ef4c690ff0d88a055e8510d0b791cd38273b
kevin chao <kchao562@gmail.com>
2017-09-27 22:55:35 -0700

    fixed db again

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,37 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 8889,
-  host: "localhost",
-  user: "root",
-  password: "Insecure",
-  database: "burgers_db"
-});
+var connection; // for heroku
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if(process.env.JAWSDB_URL){
+  connetion = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Insecure',
+    database: 'burgers_db'
+  });
+}
+
+connection.connect();
+
+// FOR LOCAL HOST
+// -----------------
+// var connection = mysql.createConnection({
+//   port: 8889,
+//   host: "localhost",
+//   user: "root",
+//   password: "Insecure",
+//   database: "burgers_db"
+// });
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 // Export connection for our ORM to use.
 module.exports = connection;
